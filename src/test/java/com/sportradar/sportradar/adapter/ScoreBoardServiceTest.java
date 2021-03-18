ff3a885a82bb2900001e9d63ce85f0f9b724ecb8
SebaLG <sebalg180@gmail.com>
2021-03-18 17:31:42 +0100

    Write finish Game failing Test

--- src/test/java/com/sportradar/sportradar/adapter/ScoreBoardServiceTest.java
+++ src/test/java/com/sportradar/sportradar/adapter/ScoreBoardServiceTest.java
@@ -35,4 +35,37 @@ public class ScoreBoardServiceTest {
     Assertions.assertEquals(0, gameAdded.getHomeTeamScore());
     Assertions.assertEquals(0, gameAdded.getAwayTeamScore());
   }
+
+  @Test
+  void finishGame_CorrectGame_RemoveInScoreBoard() {
+    // Given
+    String homeTeamName = "Uruguay";
+    String awayTeamName = "Italy";
+    Game gameToAdd =
+        Game.builder()
+            .homeTeamName(homeTeamName)
+            .awayTeamName(awayTeamName)
+            .homeTeamScore(0)
+            .awayTeamScore(0)
+            .build();
+    var games = scoreBoardService.getScoreBoard().getGames();
+    games.add(gameToAdd);
+
+    // When
+    scoreBoardService.finishGame(homeTeamName, awayTeamName);
+
+    // Then
+    Assertions.assertEquals(
+        false,
+        games.stream()
+            .anyMatch(
+                game ->
+                    game.getHomeTeamName().equals(homeTeamName)
+                        && game.getAwayTeamName().equals(awayTeamName)));
+  }
+
+
+
+
+
 }
